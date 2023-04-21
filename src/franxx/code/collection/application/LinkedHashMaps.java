ed00f2f0da34ebf4a4a521ccb3646b8d2e98ff0a
franxx <miigo@duck.com>
2023-04-21 14:24:24 +0700

    LinkedHashMaps

--- src/franxx/code/collection/application/LinkedHashMaps.java
+++ src/franxx/code/collection/application/LinkedHashMaps.java
@@ -1,7 +1,21 @@
 package franxx.code.collection.application;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class LinkedHashMaps {
     public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+
+        map.put("first", "Hilmi");
+        map.put("middle", "Akbar");
+        map.put("last", "Muharrom");
+
+        Set<String> keys = map.keySet();
+        for (var v : keys) {
+            System.out.println(v);
+        }
 
     }
 }
